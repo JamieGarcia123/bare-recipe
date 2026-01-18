7318a8fc423021d7f9203f4637dfa0a31ae74c78
Jamie Garcia <48419492+JamieGarcia123@users.noreply.github.com>
2026-01-18 09:06:26 -0600

    using layout page for meta. other options no working on home page

--- bare-recipe/app/layout.tsx
+++ bare-recipe/app/layout.tsx
@@ -6,7 +6,16 @@ import'../app/home/home.css'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <head>
+        <title>Bare Recipe | Easy Recipes. No ads, no frills, beginner cook to kid-friendly.</title>
+        <meta name="description" content="No ads, no scrolling past essays — just the bare recipe. Easy, beginner-friendly, kid-friendly recipes with clear ingredients and simple steps." />
+        <meta property="og:title" content="Bare Recipe | Easy Recipes Without the Annoying Ads" />
+        <meta property="og:description" content="Cook without scrolling past pop-ups, videos, or life stories. Just clean, simple recipes." />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://bare-recipe.com/" />
+        <meta property="og:image" content="https://bare-recipe.com/recipe-book-svg.svg" />
+              </head>
+              <body>
         <HeaderNav />
         {children}
         <Footer />
